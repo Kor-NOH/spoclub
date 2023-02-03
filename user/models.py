737065795f6e40c04f0a28aea5491fd59f70dd3c
Kor-NOH <shehddnjs1982@naver.com>
2023-02-03 15:22:33 +0900

    make DB and navibar

--- user/models.py
+++ user/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.TextField(max_length=30)   # 실명
+    id = models.TextField(primary_key=True)     # 아이디
+    pw = models.TextField()     # 비밀 번호
+    phonenum = models.IntegerField(max_length=40)   # 전화 번호
+    schoolssn = models.IntegerField(max_length=40)  # 학번
