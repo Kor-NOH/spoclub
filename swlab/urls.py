e510d25ec3acd8680ce831bfd2137b02aa8f2f3b
Kor-NOH <shehddnjs1982@naver.com>
2023-02-03 17:54:42 +0900

    making login

--- swlab/urls.py
+++ swlab/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import Sub
+from .views import Sub, Register
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',Sub.as_view())
+    path('',Sub.as_view()),
+    path('register', Register.as_view())
 ]
--- swlab/views.py
+++ swlab/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 
+
 class Sub(APIView):
     def get(self, request):
         return render(request, "swlab/main.html")
+
+class Register(APIView):
+    def get(self, request):
+        return render(request, "swlab/register.html")
