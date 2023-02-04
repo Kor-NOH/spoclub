a8ce96ad77abf69e63046938c8c25a143f2407d6
Kor-NOH <shehddnjs1982@naver.com>
2023-02-04 19:15:27 +0900

    i cant use check_password

--- swlab/views.py
+++ swlab/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from user.models import User
 from django.contrib.auth.hashers import make_password
+from django.contrib.auth.hashers import check_password
 from rest_framework.response import Response
+
 
 class Sub(APIView):
     def get(self, request):
         return render(request, "swlab/main.html")
+
 
 class Register(APIView):
     def get(self, request):
@@ -30,3 +33,17 @@ class Register(APIView):
 class Login(APIView):
     def get(self, request):
         return render(request, "swlab/login.html")
+
+    def post(self, request):
+        id = request.data.get('id', None)
+        pw = request.data.get('pw', None)
+
+        user = User.objects.filter(id=id).first()
+
+        if user is None:
+            return Response(status=400, data=dict(message="회원정보가 잘못되었습니다."))
+
+        if user.check_password(pw):
+            return Response(status=200)
+        else:
+            return Response(status=400, data=dict(message="회원정보가 잘못되었습니다."))
