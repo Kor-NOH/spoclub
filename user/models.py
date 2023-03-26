59a66a84cca10d9f6bbf96982324b6997c926711
Kor-NOH <shehddnjs1982@naver.com>
2023-03-26 16:21:03 +0900

    ch repository name

--- user/models.py
+++ user/models.py
@@ -11,8 +11,3 @@ class User(models.Model):
 
 class Images(models.Model):
     imgfile = models.ImageField(null=True, upload_to="", blank=True) # 이미지 컬럼 추가
-
-class SoccerTeam(models.Model):
-    stname = models.TextField(max_length=10)    # 축구 팀 이름
-    sport_type = models.TextField(max_length=20)    # 스포츠 종목
-    open_date = timestamp(null = False)     # 생성 날짜
