09dca4adb272e1ec5c41b1f07ff18caf0af46b6c
Kor-NOH <shehddnjs1982@naver.com>
2023-04-03 12:15:52 +0900

    mk relation table

--- user/models.py
+++ user/models.py
@@ -16,5 +16,14 @@ class SpoTeam(models.Model):
     sportevent = models.TextField(max_length=20)    # 종목
     name = models.TextField(primary_key=True, max_length=20)    # 팀 이름
     created_at = models.DateTimeField(auto_now_add=True)    # 팀 생성 시간
-    team_mark = models.ImageField(null=True)
+    team_mark = models.ImageField(null=True)    # 팀 마크
     team_intro = models.TextField(max_length=200)   # 팀 소개글
+
+class TeamRealation(models.Model):
+    sportevent = models.TextField(max_length=20) # 종목
+    teamname = models.ForeignKey('SpoTeam', on_delete=models.CASCADE, db_column='teamname') # 팀 이름
+    username = models.TextField(max_length=10)    # 소속 유저 이름
+    userid = models.ForeignKey('User', on_delete=models.CASCADE, db_column='userid')    # 소속 유저 아이디
+    position = models.TextField(max_length=20)  # 포지션
+    manager = models.BooleanField(default=False)    # 관리자인가?
+    rank = models.TextField(max_length=20)  # 직급
